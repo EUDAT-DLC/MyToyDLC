f6de6bed30b03f9e2b2c282ccda2abafc28c3281
Emanuel Dima <emanueldima@gmail.com>
2015-06-06 13:45:23 +0200

    interface mockup again, WIP

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import ipdb
 
 app = Flask(__name__)
 
@@ -6,9 +7,28 @@ app.config.update(dict(
     DEBUG=True,
 ))
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET', 'POST'])
 def root():
-    return render_template('main.html')
+    if request.method == 'POST':
+        file = request.files['file']
+        # ipdb.set_trace()
+        return file.filename
+    else:
+        return render_template('main.html')
 
 if __name__ == "__main__":
     app.run()
+
+
+# workflow-like web interface
+# drag and drop file zone
+# b2drop: manipulate files
+# select services to be used
+#     shuffling, b2note
+# call services on files
+# semantic annots via b2note
+# visualize version info (registry)
+# visualize provenance info
+#     triple store @bsc
+# push to b2share
+# geolocalization for the data/services
